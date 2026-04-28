31be7be22946ef4c0dfa6ee02d18ca0215f67906
tropicxcodes <tropicx856@gmail.com>
2026-04-28 14:45:51 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from discord import app_commands
 import asyncio
 import os
-import subprocess
 
 # Force install Chromium on startup
 from booker import book_room, check_availability
